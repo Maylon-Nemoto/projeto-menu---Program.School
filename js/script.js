d281a437d3e9c2031ab1bafd1fdf2cc3713b26e3
Maylon-Nemoto <79079037+Maylon-Nemoto@users.noreply.github.com>
2022-02-13 20:37:16 +0900

    treino simples de HTML CSS

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+let botao = document.querySelector('#botaoMaisMenu')
+
+let caixaMenu = document.querySelector('.conteudo')
+
+function Clicarbotao() {   
+    caixaMenu.innerHTML = '<h1>Diz ai Mundo!</h1>'
+}
+
+botao.addEventListener('click', botao)
